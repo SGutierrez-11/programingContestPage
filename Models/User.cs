f16558256d311715b517125e5e01111a8c690dfa
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-02-06 11:37:10 -0500

    adding some parts in the code

--- Models/User.cs
+++ Models/User.cs
@@ -7,8 +7,11 @@ namespace programingContestPage.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string LastName { get; set; }
+        [Required]
         public string UserName { get; set; }
 
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [DataType(DataType.DateTime)]
---
+++ Pages/Account/Login.cshtml.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using programingContestPage.Models;
+
+namespace programingContestPage.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public User users { get; set; }
+        public void OnGet()
+        {
+            
+        }
+        public void OnPost()
+        {
+
+        }
+
+    }
+}
